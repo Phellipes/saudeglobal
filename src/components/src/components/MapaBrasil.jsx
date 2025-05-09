2c03af5a755106e9ca81d46a8c2872bdfc5c581f
Phellipes <ewertonphellip1906@gmail.com>
2025-05-09 15:41:58 -0300

    Create MapaBrasil.jsx

---
+++ src/components/src/components/MapaBrasil.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapaBrasil = () => {
+  useEffect(() => {
+    const map = L.map('map').setView([-15.788, -47.875], 4); // Centro do Brasil
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    fetch('https://raw.githubusercontent.com/tbrugz/geodata-br/master/geojson/geojs-13-mun.json ')
+      .then(res => res.json())
+      .then(data => {
+        L.geoJSON(data).addTo(map);
+      });
+
+    return () => map.remove();
+  }, []);
+
+  return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
+};
+
+export default MapaBrasil;
