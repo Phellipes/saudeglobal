5f092eb170d78f9bf9e5be448c6858771fc29735
Phellipes <ewertonphellip1906@gmail.com>
2025-05-09 15:43:18 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+import Relatorios from './components/Relatorios';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <main style={{ marginLeft: '250px', padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/relatorios" element={<Relatorios />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
